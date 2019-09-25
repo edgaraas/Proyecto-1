90a79ac7bf7b1777099bb0f6063be07382602e11
Edgar Alvarez <edgaraas98@gmai.com>
2019-09-25 18:59:38 -0500

    "usando datos desde views"

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Bienvenido a Automart</h1>')
+    nombre = "VW Jetta"
+    modelo = 2018
+    precio = "$ 153,698.00" 
+    color = "Gris"
+
+    dict = { 'auto_nombre' : nombre,
+            'auto_modelo' : modelo,
+            'auto_precio' : precio,
+            'auto_color' : color
+    }
+    return render(request, 'index.html', dict)
